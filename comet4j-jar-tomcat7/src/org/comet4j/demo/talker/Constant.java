8427e9451c40ff512c1fd7bbefd5e1a506b04a00
jinghai.xiao@gmail.com <jinghai.xiao@gmail.com@c836c6bf-4277-0927-0d25-ca52ea04c95c>
2011-03-21 08:58:52 +0000

    调整包名

---
+++ comet4j-jar-tomcat7/src/org/comet4j/demo/talker/Constant.java
@@ -0,0 +1,15 @@
+package org.comet4j.demo.talker;
+
+public class Constant {
+
+	public static final String APP_MODULE_KEY = "talker";
+
+	// 上线消息标识
+	public static final String UP = "up";
+	// 下线消息标识
+	public static final String DOWN = "down";
+	// 改名消息标识
+	public static final String RENAME = "rename";
+	// 话语消息标识
+	public static final String TALK = "talk";
+}
