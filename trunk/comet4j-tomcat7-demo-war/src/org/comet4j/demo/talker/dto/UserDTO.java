293baffab351115bb1513b001606517baef37f2a
jinghai.xiao@gmail.com <jinghai.xiao@gmail.com@c836c6bf-4277-0927-0d25-ca52ea04c95c>
2011-04-07 17:53:21 +0000

    demo改进

---
+++ trunk/comet4j-tomcat7-demo-war/src/org/comet4j/demo/talker/dto/UserDTO.java
@@ -0,0 +1,39 @@
+/*
+ * Comet4J Copyright(c) 2011, http://code.google.com/p/comet4j/ This code is
+ * licensed under BSD license. Use it as you wish, but keep this copyright
+ * intact.
+ */
+package org.comet4j.demo.talker.dto;
+
+/**
+ * 用户信息传输对象
+ * @author jinghai.xiao@gmail.com
+ * @date 2011-4-7
+ */
+public class UserDTO {
+
+	private String id;
+	private String name;
+
+	public UserDTO(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
