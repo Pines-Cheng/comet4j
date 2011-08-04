b7fcee4cca33c2b8219fbf2a73723bfdbbbfdceb
jinghai.xiao@gmail.com <jinghai.xiao@gmail.com@c836c6bf-4277-0927-0d25-ca52ea04c95c>
2011-08-04 19:25:11 +0000

    修复Tomcat6下NIOEndPoint异常问题

--- comet4j-jar/src/org/comet4j/core/CometServletTomcat6.java
+++ comet4j-jar/src/org/comet4j/core/CometServletTomcat6.java
@@ -33,32 +33,40 @@ public class CometServletTomcat6 extends HttpServlet implements CometProcessor {
 	public void event(CometEvent event) throws IOException, ServletException {
 		HttpServletRequest request = event.getHttpServletRequest();
 		HttpServletResponse response = event.getHttpServletResponse();
-		request.setAttribute("org.apache.tomcat.comet.timeout", CometContext.getInstance().getTimeout());
+		request.setAttribute("org.apache.tomcat.comet.timeout", CometContext
+				.getInstance().getTimeout());
 		event.setTimeout(CometContext.getInstance().getTimeout());
 		if (event.getEventType() == CometEvent.EventType.BEGIN) {
 			String action = request.getParameter(CometProtocol.FLAG_ACTION);
 
 			if (CometProtocol.CMD_CONNECT.equals(action)) {
 
-				CometContext.getInstance().getEngine().connect(request, response);
+				CometContext.getInstance().getEngine()
+						.connect(request, response);
+				event.close();
 			} else if (CometProtocol.CMD_REVIVAL.equals(action)) {
 
-				CometContext.getInstance().getEngine().revival(request, response);
+				CometContext.getInstance().getEngine()
+						.revival(request, response);
 
 			} else if (CometProtocol.CMD_DROP.equals(action)) {
 				CometContext.getInstance().getEngine().drop(request, response);
+				event.close();
 			}
 		} else if (event.getEventType() == CometEvent.EventType.ERROR) {
 			if (event.getEventSubType() == CometEvent.EventSubType.TIMEOUT) {
 				CometContext.getInstance().getEngine().dying(request, response);
+				event.close();
 			} else {
 
 				CometContext.getInstance().getEngine().drop(request, response);
+				event.close();
 			}
 
 		} else if (event.getEventType() == CometEvent.EventType.END) {
 
 			CometContext.getInstance().getEngine().dying(request, response);
+			event.close();
 		} else if (event.getEventType() == CometEvent.EventType.READ) {
 
 		}
